98f41e4af32af3cf63d796b6b21f2860147c88a3
Jiri <jvanek@redhat.com>
2019-09-02 08:46:47 +0200

    Inital maven init

---
+++ src/main/java/org/judovana/callendarmaker/App.java
@@ -0,0 +1,13 @@
+package org.judovana.callendarmaker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
