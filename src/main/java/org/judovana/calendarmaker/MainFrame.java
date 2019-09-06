daf7c16f6c20271cdf36868750d53ddd855c261b
Jiri <jvanek@redhat.com>
2019-09-06 20:11:54 +0200

    Added wheel listener and brainstroming popupmenu

--- src/main/java/org/judovana/calendarmaker/MainFrame.java
+++ src/main/java/org/judovana/calendarmaker/MainFrame.java
@@ -2,8 +2,7 @@ package org.judovana.calendarmaker;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.io.IOException;
 import java.util.*;
 import java.util.List;
@@ -20,14 +19,33 @@ public class MainFrame extends JFrame {
         RangeProvider rp = new RangeProvider(2019, false);
         List<List<Date>> ranges = rp.getRanges();
         List<PageView> pages = new ArrayList<>(ranges.size());
-        for (List<Date> range : ranges){
-                CalendarPage cp = new CalendarPage(range, new PhotoFrame(pl.getRandomImage()));
-                PageView p =new PageView(cp);
-                pages.add(p);
+        for (List<Date> range : ranges) {
+            CalendarPage cp = new CalendarPage(range, new PhotoFrame(pl.getRandomImage()));
+            PageView p = new PageView(cp);
+            pages.add(p);
         }
 
         final AllView all = new AllView(pages);
+        all.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JPopupMenu menu = new JPopupMenu();
+                menu.add(new JMenuItem("Select"));
+                menu.add(new JMenuItem("previous"));
+                menu.add(new JMenuItem("next"));
+                menu.add(new JMenuItem("random again"));
+                menu.add(new JMenuItem("rotate"));
+                menu.show(all, e.getX(), e.getY());
+
+            }
+        });
         this.add(all);
+        this.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                all.adjsutOffset(e.getUnitsToScroll() * 20);
+            }
+        });
         this.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent keyEvent) {
@@ -43,10 +61,10 @@ public class MainFrame extends JFrame {
                     all.adjsutOffset(+MainFrame.this.getHeight() / 3);
                 }
                 if (keyEvent.getKeyCode() == KeyEvent.VK_UP) {
-                    all.adjsutOffset(-1);
+                    all.adjsutOffset(+1);
                 }
                 if (keyEvent.getKeyCode() == KeyEvent.VK_DOWN) {
-                    all.adjsutOffset(+1);
+                    all.adjsutOffset(-1);
                 }
             }
 
