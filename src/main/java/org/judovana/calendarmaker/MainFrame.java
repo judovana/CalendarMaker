88593abf7ff9d51b8aaac8214490c5b5edf4aabd
Jiri <jvanek@redhat.com>
2019-09-06 15:28:25 +0200

    Fixed bug wwith week starting in monday

--- src/main/java/org/judovana/calendarmaker/MainFrame.java
+++ src/main/java/org/judovana/calendarmaker/MainFrame.java
@@ -10,17 +10,23 @@ import java.util.List;
 
 public class MainFrame extends JFrame {
 
+    public static String[] PhotoFolders = new String[]{"/home/jvanek/tripshare/Context/Data/Fotky", "/usr/share/backgrounds"};
+
     public MainFrame() throws IOException {
         this.setSize(800, 600);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         PhotoLoader pl = new PhotoLoader();
 
-        int yearOfChoice=2019;
-        boolean week = true;
+        int yearOfChoice = 2019;
+        boolean week = false;
 
         Calendar c2 = new GregorianCalendar();
         c2.set(yearOfChoice, 0, 1);
         rewindToStartofWeek(c2);
+        //we are on monday
+        c2.add(Calendar.DAY_OF_YEAR, -1);
+        //so we canadd in loop
+
         List<PageView> pages = new ArrayList<>(60);
 
         List<Date> dates = new ArrayList<>(40);
@@ -30,7 +36,7 @@ public class MainFrame extends JFrame {
                 dates.add(theDay);
                 c2.add(Calendar.DAY_OF_YEAR, 1);
                 if (dates.size() >= 7) {
-                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage("/usr/share/backgrounds"))));
+                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage(PhotoFolders))));
                     pages.add(pv);
                     dates = new ArrayList<>(40);
                 }
@@ -40,13 +46,13 @@ public class MainFrame extends JFrame {
                         dates.add(theTheDay);
                         c2.add(Calendar.DAY_OF_YEAR, 1);
                     }
-                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage("/usr/share/backgrounds"))));
+                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage(PhotoFolders))));
                     pages.add(pv);
                     break;
                 }
             }
         } else {
-            int lastMonth=0;
+            int lastMonth = 0;
             while (true) {
                 Date theDay = c2.getTime();
                 dates.add(theDay);
@@ -58,7 +64,7 @@ public class MainFrame extends JFrame {
                         dates.add(theTheDay);
                         c2.add(Calendar.DAY_OF_YEAR, +1);
                     }
-                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage("/usr/share/backgrounds"))));
+                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage(PhotoFolders))));
                     pages.add(pv);
                     dates = new ArrayList<>(40);
                     rewindToStartofWeek(c2);
@@ -69,7 +75,7 @@ public class MainFrame extends JFrame {
                         dates.add(theTheDay);
                         c2.add(Calendar.DAY_OF_YEAR, +1);
                     }
-                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage("/usr/share/backgrounds"))));
+                    PageView pv = new PageView(new CalendarPage(dates, new PhotoFrame(pl.getRandomImage(PhotoFolders))));
                     pages.add(pv);
                     break;
                 }
