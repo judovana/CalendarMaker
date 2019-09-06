a236c340f1f19cce24c907cce03e958afc3f425c
Jiri <jvanek@redhat.com>
2019-09-06 15:28:41 +0200

    Made highlights on month changes

--- src/main/java/org/judovana/calendarmaker/CalendarPage.java
+++ src/main/java/org/judovana/calendarmaker/CalendarPage.java
@@ -1,6 +1,7 @@
 package org.judovana.calendarmaker;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.Date;
 import java.util.List;
@@ -18,14 +19,24 @@ public class CalendarPage {
     public void paint(Graphics2D g, int x, int y, int w, int h) {
         dates.draw(x, y, w / 2, h, g);
         photo.draw(x + w / 2, y, w / 2, h, g);
+
+
         String title = dates.getTitle();
+        Rectangle2D rect1 = g.getFontMetrics().getStringBounds(title, g);
         int th = g.getFontMetrics().getHeight();
-        int tw = (int) g.getFontMetrics().getStringBounds(title, g).getWidth();
+        int tw = (int) rect1.getWidth();
+        g.setColor(new Color(255, 255, 225, 125));
+        g.fillRect(w / 2 + w / 4 - tw / 2, y, tw, th);
+        g.setColor(Color.black);
         g.drawString(title, w / 2 + w / 4 - tw / 2, y + th);
 
         String footer = photo.getFooter();
+        Rectangle2D rect2 = g.getFontMetrics().getStringBounds(footer, g);
         int fh = g.getFontMetrics().getHeight();
-        int fw = (int) g.getFontMetrics().getStringBounds(footer, g).getWidth();
+        int fw = (int) rect2.getWidth();
+        g.setColor(new Color(255, 255, 225, 125));
+        g.fillRect(w / 2 + w / 4 - fw / 2, y + h - 2 * fh, fw, fh);
+        g.setColor(Color.black);
         g.drawString(footer, w / 2 + w / 4 - fw / 2, y + h - fh);
     }
 }
--- src/main/java/org/judovana/calendarmaker/DateRangeRenderrer.java
+++ src/main/java/org/judovana/calendarmaker/DateRangeRenderrer.java
@@ -19,22 +19,41 @@ public class DateRangeRenderrer {
     public static final SimpleDateFormat thisMonth = new SimpleDateFormat("EEE dd.MM");
 
     public static final SimpleDateFormat monthName = new SimpleDateFormat("MMMMMMMM yyyy");
-    public static final SimpleDateFormat dayThis= new SimpleDateFormat("EEEEEEEE dd.MM");
+    public static final SimpleDateFormat dayThis = new SimpleDateFormat("EEEEEEEE dd.MM");
 
     public void draw(int x, int y, int w, int h, Graphics2D g) {
         int border = 5;
+        g.setColor(Color.black);
         if (range.size() == 7) {
-            int step = h / range.size();
+            int step = (h - (2 * border)) / range.size();
             g.drawRect(x + border, y + border, w - 2 * border, h - 2 * border);
             for (int i = 0; i < range.size(); i++) {
-                int hh = border + (i + 1) * step;
-                g.drawString(dayThis.format(range.get(i)), 2 * border, y + hh - (step - g.getFontMetrics().getHeight()));
-                g.drawLine(border, y + hh, w - border, y + hh);
+                int hh = y + border + (i) * step;
+                g.drawString(dayThis.format(range.get(i)), 2 * border, hh - (step - g.getFontMetrics().getHeight()));
+                g.setColor(Color.black);
+                if (i < range.size() - 1) {
+                    Calendar c1 = new GregorianCalendar();
+                    c1.setTime(range.get(i + 1));
+                    Calendar c2 = new GregorianCalendar();
+                    c2.setTime(range.get(i));
+                    if (c1.get(Calendar.MONTH) != c2.get(Calendar.MONTH)) {
+                        g.setColor(Color.red);
+                    }
+                }
+                g.drawLine(border, hh, w - border, hh);
             }
         } else {
             int ww = (w - (2 * border)) / 7;
             int hh = (h - (2 * border)) / (range.size() / 6);
+            Calendar c1 = new GregorianCalendar();
+            c1.setTime(range.get(range.size() / 2));
             for (int i = 0; i < range.size(); i++) {
+                Calendar c2 = new GregorianCalendar();
+                c2.setTime(range.get(i));
+                g.setColor(Color.black);
+                if (c2.get(Calendar.MONTH) != c1.get(Calendar.MONTH)) {
+                    g.setColor(new Color(0, 0, 0, 125));
+                }
                 int row = (i / 7);
                 int inRow = (i % 7);
                 g.drawRect(border + inRow * ww, y + border + row * hh, ww, hh);
@@ -49,10 +68,10 @@ public class DateRangeRenderrer {
             cS.setTime(range.get(0));
             Calendar cE = new GregorianCalendar();
             cE.setTime(range.get(range.size() - 1));
-            if (cS.get(Calendar.MONTH) == cE.get(Calendar.MONTH)){
+            if (cS.get(Calendar.MONTH) == cE.get(Calendar.MONTH)) {
                 Date midle = range.get(range.size() / 2);
                 return monthName.format(midle);
-            }else {
+            } else {
 
                 return monthName.format(cS.getTime()) + " - " + monthName.format(cE.getTime());
             }
